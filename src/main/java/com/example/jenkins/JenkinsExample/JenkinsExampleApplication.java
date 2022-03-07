35a5cf8ed5b614ff0f737a0d1f497c0655a3f0a8
mahendra <jmahendra25@gmail.com>
2022-03-07 13:07:30 +0530

    changes

--- src/main/java/com/example/jenkins/JenkinsExample/JenkinsExampleApplication.java
+++ src/main/java/com/example/jenkins/JenkinsExample/JenkinsExampleApplication.java
@@ -3,6 +3,8 @@ package com.example.jenkins.JenkinsExample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class JenkinsExampleApplication {
 
